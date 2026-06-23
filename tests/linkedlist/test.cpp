33e89847a5a16ab6fdb569927fd2fd3f552f16d5
garvv29 <garv.choure2023@ssipmt.com>
2026-06-24 01:38:20 +0530

    LinkedList implementation with most of the methods was done

---
+++ tests/linkedlist/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SinglyList>
+
+int main()
+{
+    SinglyList<int> l;
+    l.insert(0,10);
+    l.insert(0,11);
+    l.insert(1,12);
+    l.insert(3,13);
+    l.print();
+}
